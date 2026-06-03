c7f586ac555bec474d6bd456d0c7eb8327a93643
StrayJF3 <juangfdez3@gmail.com>
2026-06-02 23:37:12 -0500

    Add method to find Restaurante by badgeText

--- backend/app/catalogo/src/main/java/com/ajigo/catalogo/repository/RestauranteRepository.java
+++ backend/app/catalogo/src/main/java/com/ajigo/catalogo/repository/RestauranteRepository.java
@@ -1,12 +1,13 @@
 package com.ajigo.catalogo.repository;
 
-import com.ajigo.catalogo.model.*;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
+import com.ajigo.catalogo.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
     List<Restaurante> findByActivoTrue();
+    Optional<Restaurante> findByBadgeText(String badgeText);
 }
